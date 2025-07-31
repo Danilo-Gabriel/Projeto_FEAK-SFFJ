b855a059d1e8d4559f09c64d4a438b35c8d286e7
Danilo Gabriel <danilonascimento.dr@gmail.com>
2025-07-31 09:17:44 -0300

    feature: implementar interface de login

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { LibsModule } from '../assets/libs/libs.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LibsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
