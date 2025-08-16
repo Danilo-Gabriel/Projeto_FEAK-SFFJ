f99dbed3614cd22b44abae89870934531152d174
Mateus <m.nunesaj@gmail.com>
2025-08-16 12:02:53 -0300

    alteração estoque

--- web/src/app/pages/routing/pages-routing.module.ts
+++ web/src/app/pages/routing/pages-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsuarioComponent } from '../usuario/usuario.component';
 import { PagesComponent } from '../pages.component';
+import { EstoqueComponent } from '../estoque/estoque.component';
 
 const routes: Routes = [
     {path: '', component: PagesComponent,
 
     children: [
     { path: 'usuario', component: UsuarioComponent},
+    { path: 'estoque', component: EstoqueComponent},
+
 
     ]
   }
