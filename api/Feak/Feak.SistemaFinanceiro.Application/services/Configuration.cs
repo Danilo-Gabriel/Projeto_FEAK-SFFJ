3cb78885ed12c69de35721f466c785dd049fa66f
Danilo Gabriel <danilonascimento.dr@gmail.com>
2025-09-21 00:53:24 -0300

    initial-commit

---
+++ api/Feak/Feak.SistemaFinanceiro.Application/services/Configuration.cs
@@ -0,0 +1,36 @@
+﻿using DomainService.Interfaces;
+using DomainService.Interfaces.Services;
+using DomainService.services;
+using Feak.SistemaFinanceiro.Persistencia.Context;
+using Feak.SistemaFinanceiro.Persistencia.services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.services;
+
+public static class Configuration
+{
+    public static void AddContextsServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("feak");
+
+            services.AddDbContext<ApplicationDbcontext>(options =>
+            options.UseNpgsql(connectionString));
+        
+    }
+
+    public static void AddServices(this IServiceCollection services)
+    {
+        // Domain services
+        services.AddTransient(typeof(IBaseDomainService<>),  typeof(BaseDomainService<>));
+        services.AddTransient<IUsuarioDomainService, UsuarioDomainService>();
+        
+        // Repository
+        services.AddTransient(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+        services.AddTransient<IUsuarioRepository, UsuarioRepository>();
+    }
+    
+    
+    
+}
---
+++ api/Feak/Feak.SistemaFinanceiro.DomainService/Interfaces/Services/IBaseDomainService.cs
@@ -0,0 +1,13 @@
+﻿using DomainService.Entities;
+
+namespace DomainService.Interfaces.Services;
+
+public interface IBaseDomainService<TEntity> where TEntity : class
+{
+    Task<TEntity> CreateAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+    Task<TEntity> DeleteAsync (TEntity entity);
+    
+    Task<TEntity> GetByIdAsync(Guid id);
+    Task<List<TEntity>> GetAllAsync();
+}
